fed926ba2e49f5accba26ede48ba4bcfcdd6b379
David <rafael.santos.jb22@gmail.com>
2024-06-20 17:46:45 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form').addEventListener('submit', function(e) {
+        e.preventDefault()
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt(numeroMaximo);
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    })
+})
